44cf08b2a7739540fa862c9e9ff82e6dff27fff0
lud <ludovic@demblans.com>
2025-01-07 09:57:06 +0100

    (fix ci cache)

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -4,6 +4,5 @@
   {"lib/jsv/format_validator/default/optional/json_pointer.ex", :extra_range},
   {"lib/jsv/format_validator/default/optional/uri.ex", :unused_fun},
   {"lib/jsv/format_validator/default/optional/uri.ex", :extra_range},
-  {"lib/jsv/format_validator/default/optional/uri_template.ex", :extra_range},
-  {"lib/jsv/format_validator/default/optional/uri_template.ex", :unused_fun}
+  {"lib/jsv/format_validator/default/optional/uri_template.ex", :extra_range}
 ]
