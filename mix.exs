e9a308418c4067792c005c75933c332647deeb87
lud <ludovic@demblans.com>
2025-03-28 18:31:44 +0100

    chore: Release v0.5.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule JSV.MixProject do
     [
       app: :jsv,
       description: "A JSON Schema Validator with complete support for the latest specifications.",
-      version: "0.5.0",
+      version: "0.5.1",
       elixir: "~> 1.15",
       # no protocol consolidation for the generation of the test suite
       consolidate_protocols: false,
