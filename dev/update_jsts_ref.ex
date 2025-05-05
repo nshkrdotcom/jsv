9d8624a7ffe4c089f7eefc1451a8cd63bd3415e7
lud <ludovic@demblans.com>
2025-05-05 17:04:04 +0200

    chore: Enhanced JSTS updater

--- dev/update_jsts_ref.ex
+++ dev/update_jsts_ref.ex
@@ -8,21 +8,40 @@ defmodule Mix.Tasks.Jsv.UpdateJstsRef do
   def run(_argv) do
     IO.puts("pulling main on #{@dep_dir}")
 
-    {_, 0} = System.cmd("git", ~w(reset --hard HEAD), cd: @dep_dir)
-    {_, 0} = System.cmd("git", ~w(checkout main), cd: @dep_dir)
-    {_, 0} = System.cmd("git", ~w(pull), cd: @dep_dir)
-    {ref, 0} = System.cmd("git", ~w(rev-parse HEAD), cd: @dep_dir)
-    ref = String.trim(ref)
+    current_ref = current_ref()
+    {_, 0} = git(~w(reset --hard HEAD))
+    {_, 0} = git(~w(checkout main))
+    {_, 0} = git(~w(pull))
+    {new_ref, 0} = git(~w(rev-parse HEAD))
+    new_ref = String.trim(new_ref)
+
+    if new_ref == current_ref do
+      Mix.Shell.IO.error("JSON Schema Test Suite already up-to-date on #{new_ref}")
+      System.halt(1)
+    end
+
+    {log, 0} = git(~w(log #{current_ref}..#{new_ref}))
+    IO.puts([IO.ANSI.faint(), log, IO.ANSI.reset()])
 
     IO.puts("updating mix.exs")
 
     "mix.exs"
     |> File.read!()
-    |> String.replace(~r/@jsts_ref "[^"]+"/, ~s(@jsts_ref "#{ref}"))
+    |> String.replace(~r/@jsts_ref "[^"]+"/, ~s(@jsts_ref "#{new_ref}"))
     |> then(&File.write!("mix.exs", &1))
 
     IO.puts("done")
   end
 
-  # Format can be :binary or :atom, it changes the way the schemas will be
+  defp current_ref do
+    Mix.Project.config()
+    |> Keyword.fetch!(:deps)
+    |> List.keyfind!(:json_schema_test_suite, 0)
+    |> elem(1)
+    |> Keyword.fetch!(:ref)
+  end
+
+  defp git(argv) do
+    System.cmd("git", argv, cd: @dep_dir)
+  end
 end
